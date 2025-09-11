51027b1c5e37348f65de90cd628edfdb6301b28b
HamzaBilal527 <hamzabilal527@gmail.com>
2025-09-12 04:27:32 +0500

    New Chatbot V4.0 Patch 7

--- HMS/paitentdoctorsystembackend/server.js
+++ HMS/paitentdoctorsystembackend/server.js
@@ -35,7 +35,7 @@ app.use("/api/admin", adminRouter)
 app.use("/api/doctor", doctorRouter)
 app.use("/api/get-speciality", getSpecialityRoute)
 app.use('/api/specialties', specialtiesRouter);
-// app.use('/api/ai', aiChatRouter);
+app.use('/api/ai', aiChatRAG);
 app.use('/api', aiChatRAG);
 
 app.get("/api", (req, res) => {
